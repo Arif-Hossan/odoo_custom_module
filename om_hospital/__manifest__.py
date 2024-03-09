7f8414596b0827a9b4b85122944573e8149252cb
Arif Hossan <arif.hossan.eu@gmail.com>
2024-03-10 03:53:05 +0600

    Hospital management module created, with model, and form view

---
+++ om_hospital/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+{
+    'name': 'Hospital Management',
+    'version': '1.0',
+    'summary': 'Hospital Management Software',
+    'sequence': -100,
+    'description': """Hospital Management Software""",
+    'category': 'Productivity',
+    'website': 'http://arkap.ezyro.com/',
+    'images': [],
+    'depends': [],
+    'data': [
+        'security/ir.model.access.csv',
+        'views/patient.xml'
+    ],
+    'demo': [],
+    'qweb': [],
+    'installable': True,
+    'application': True,
+    'auto_install': False,
+    'license': 'LGPL-3',
+}
